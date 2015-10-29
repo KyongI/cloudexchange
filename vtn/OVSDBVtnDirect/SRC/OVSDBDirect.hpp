bc6372b229c1025ea37b2542447760096c731206
djkim <djkim@INNOCORE.(none)>
2015-10-29 11:24:36 +0900

    Change filename for C++ class porting

---
+++ vtn/OVSDBVtnDirect/SRC/OVSDBDirect.hpp
@@ -0,0 +1,26 @@
+#ifndef _OVSDBDIRECT_HPP_
+#define _OVSDBDIRECT_HPP__
+
+#define	READ_BUFFER_SIZE     512
+#define SEARCH_TYPE_ALL      0x01
+#define SEARCH_TYPE_MDN      0x02
+#define IPMD_TOTAL_LENGTH_LEN           4//addk
+
+class COVSDBDirect 
+{
+	public :
+		COVSDBDirect();
+		~COVSDBDirect();
+
+
+	private :
+		void ConvField( char *_buf, char * _data, int _len);
+		bool decoding(char *_fname);
+		bool MakeData(char * _fname, int _seq, char * _buf, int len);
+		void printHelpMessage();
+		void procOnlyMsg(char *inImsiFileName, int recordSeq, char *s, int isSearch, int isDetail, char *strSearch, int r);//addk
+		void GetFormatInfo(char * pstrFormatname, char * nstructCd, int isDetail );
+		void usage(char *s);
+}
+
+#endif
