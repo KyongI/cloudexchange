0c2c3ca10eb7e104830e0546bd7504d449ff5be2
KyongI <kiku4@kinx.net>
2015-11-06 11:32:19 +0900

    implement API for neutron port

--- controller/opendaylight/cloudexchange/src/main/java/org/opendaylight/controller/cloudexchange/Activator.java
+++ controller/opendaylight/cloudexchange/src/main/java/org/opendaylight/controller/cloudexchange/Activator.java
@@ -9,8 +9,36 @@ import org.slf4j.LoggerFactory;
 
 public class Activator extends ComponentActivatorAbstractBase {
 
-        private static final Logger log = LoggerFactory.getLogger(CloudExchange.class);
+	private static final Logger log = LoggerFactory.getLogger(CloudExchange.class);
 
+	/**
+	 * Function called when the activator starts just after some
+	 * initializations are done by the
+	 * ComponentActivatorAbstractBase.
+	 *
+	 */
+	public void init() {
+
+	}
+
+	/**
+	 * Function called when the activator stops just before the
+	 * cleanup done by ComponentActivatorAbstractBase
+	 *
+	 */
+	public void destroy() {
+
+	}
+
+	/**
+	 * Function that is used to communicate to dependency manager the
+	 * list of known implementations for services inside a container
+	 *
+	 *
+	 * @return An array containing all the CLASS objects that will be
+	 * instantiated in order to get an fully working implementation
+	 * Object
+	 */	
         public Object[] getImplementations() {
                 log.trace("Getting Implementations");
 
@@ -18,6 +46,19 @@ public class Activator extends ComponentActivatorAbstractBase {
                 return res;
         }
 
+	/**
+	 * Function that is called when configuration of the dependencies
+	 * is required.
+	 *
+	 * @param c dependency manager Component object, used for
+	 * configuring the dependencies exported and imported
+	 * @param imp Implementation class that is being configured,
+	 * needed as long as the same routine can configure multiple
+	 * implementations
+	 * @param containerName The containerName being configured, this allow
+	 * also optional per-container different behavior if needed, usually
+	 * should not be the case though.
+	 */
         public void configureInstance(Component c, Object imp, String containerName) {
                 log.trace("Configuring instance");
 
@@ -29,4 +70,5 @@ public class Activator extends ComponentActivatorAbstractBase {
                         props.put("salListenerName", "cloudexchange");
                 }
         }
+
 }
--- controller/opendaylight/cloudexchange/src/main/java/org/opendaylight/controller/cloudexchange/CloudExchange.java
+++ controller/opendaylight/cloudexchange/src/main/java/org/opendaylight/controller/cloudexchange/CloudExchange.java
@@ -1,13 +1,139 @@
 package org.opendaylight.controller.cloudexchange;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+                                                 
+import org.opendaylight.ovsdb.lib.table.Interface;
+import org.opendaylight.controller.networkconfig.neutron.INeutronNetworkCRUD;
+import org.opendaylight.controller.networkconfig.neutron.INeutronPortCRUD;
+import org.opendaylight.controller.networkconfig.neutron.NeutronCRUDInterfaces;
+import org.opendaylight.controller.networkconfig.neutron.NeutronPort;
+import org.opendaylight.controller.sal.utils.ServiceHelper;
+import org.opendaylight.controller.sal.utils.IObjectReader;
+import org.opendaylight.controller.sal.utils.Status;
+
 public class CloudExchange {
 
+
+	//private INeutronPortCRUD neutronPortService = (INeutronPortCRUD)ServiceHelper.getGlobalInstance(INeutronPortCRUD.class, this);
+	private INeutronPortCRUD portInterface = NeutronCRUDInterfaces.getINeutronPortCRUD(this);
+
         public void start() {
-                System.out.println("CloudExchange API service Init.");
+                System.out.println("CloudExchange API service Start.");
         }
 
         public void stop() {
                 System.out.println("CloudExchange API service Terminate");
         }
+
+	/**
+	 * Applications call this interface method to determine if a particular
+	 * Port object exists
+	 *
+	 * @param uuid
+	 *            UUID of the Port object
+	 * @return boolean
+	 */
+	public boolean CE_portExists(String uuid) {
+		return neutronPortService.portExists(uuid);
+	}
+
+	/**
+	 * Applications call this interface method to return if a particular
+	 * Port object exists
+	 *
+	 * @param uuid
+	 *            UUID of the Port object
+	 * @return {@link org.opendaylight.controller.networkconfig.neutron.NeutronPort.OpenStackPorts}
+	 *          OpenStack Port class
+	 */
+	public NeutronPort CE_getPort(String uuid) {
+		return neutronPortService.getPort(uuid);
+	}
+
+	/**
+	 * Applications call this interface method to return all Port objects
+	 *
+	 * @return List of OpenStackPorts objects
+	 */
+	public List<NeutronPort> CE_getAllPorts() {
+		return neutronPortService.getAllPorts();
+	}
+
+	/**
+	 * Applications call this interface method to add a Port object to the
+	 * concurrent map
+	 *
+	 * @param input
+	 *            OpenStackPort object
+	 * @return boolean on whether the object was added or not
+	 */	
+	public boolean CE_addPort(NeutronPort input) {
+		return neutronPortService.addPort(input);
+	}
+
+	/**
+	 * Applications call this interface method to remove a Port object to the
+	 * concurrent map
+	 *
+	 * @param uuid
+	 *            identifier for the Port object
+	 * @return boolean on whether the object was removed or not
+	 */
+	public boolean CE_removePort(String uuid) {
+		return neutronPortService.removePort(uuid);
+	}
+
+	/**
+	 * Applications call this interface method to edit a Port object
+	 *
+	 * @param uuid
+	 *            identifier of the Port object
+	 * @param delta
+	 *            OpenStackPort object containing changes to apply
+	 * @return boolean on whether the object was updated or not
+	 */
+	public boolean CE_updatePort(String uuid, NeutronPort delta) {
+		return neutronPortService.updatePort(uuid, delta);
+	}
+
+	/**
+	 * Applications call this interface method to see if a MAC address is in use
+	 *
+	 * @param macAddress
+	 *            mac Address to be tested
+	 * @return boolean on whether the macAddress is already associated with a
+	 * port or not
+	 */
+	public boolean CE_macInUse(String macAddress) {
+		return neutronPortService.macInUse(macAddress);
+	}
+
+	/**
+	 * Applications call this interface method to retrieve the port associated with
+	 * the gateway address of a subnet
+	 *
+	 * @param subnetUUID
+	 *            identifier of the subnet
+	 * @return OpenStackPorts object if the port exists and null if it does not
+	 */
+	public NeutronPort CE_getGatewayPort(String subnetUUID) {
+		return neutronPortService.getGatewayPort(subnetUUID);
+	}
+
+	
+	/*
+	public Status CE_saveConfiguration() {
+		return neutronPortService.saveConfiguration();
+	}
+
+	public Object CE_readObject(ObjectInputStream ois) throws FileNotFoundException, IOException, ClassNotFoundException {
+		return neutronPortService.readObject(ois);
+	}
+	*/
+	
 }
+
+
 
